10376723638451eccfc5633b5c437d40b261869d
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-04 16:19:46 +0300

    add  one test

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.locators import BasketPageLocators
+import time
 
 def test_guest_can_go_to_login_page(browser) -> None:
     link = "http://selenium1py.pythonanywhere.com"
@@ -15,3 +17,13 @@ def test_guest_should_see_login_link(browser) -> None:
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser) -> None:
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
+    page.open()
+    page.open_basket()
+    assert page.is_element_present(*BasketPageLocators.CONTINUE_BUY),\
+        "Your basket is not empty"
+
