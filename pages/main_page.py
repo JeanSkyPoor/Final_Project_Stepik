eea046a06dbe655b87eb299bfffa75a8c431968c
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-04 17:31:00 +0300

    update file

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,4 @@
 from pages.base_page import BasePage
-from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
-from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
