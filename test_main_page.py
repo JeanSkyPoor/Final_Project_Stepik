a52816f5ee1da6725469ab34450ac7a0b4da7abd
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-04 14:06:03 +0300

    update file

--- test_main_page.py
+++ test_main_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page(browser) -> None:
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
@@ -10,7 +10,7 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_login_link(browser) -> None:
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
