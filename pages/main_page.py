5c7413be30b2d1a3e6a878eb4282eaf325898406
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-04 13:21:01 +0300

    create should_be_login_link

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,13 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from pages.locators import MainPageLocators
+
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
+
+    def should_be_login_link(self):
+        self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not presented'
+
