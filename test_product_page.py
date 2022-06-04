ed21974d5900d647bbba0a3373b9d73c95253a35
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-04 14:05:53 +0300

    add check_item_name and check_item_price

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,3 @@
-from multiprocessing.connection import wait
 from pages.product_page import ProductPage
 import time
 
@@ -8,5 +7,6 @@ def test_guest_can_add_product_to_basket(browser) -> None:
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    while True:
-        time.sleep(1)
+    page.check_item_name()
+    page.check_item_price()
+    
