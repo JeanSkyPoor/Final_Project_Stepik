f13aaadd8950d84adf4763e1aead759188d49944
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-04 13:21:29 +0300

    create locators.py

---
+++ pages/locators.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+
+class LoginPageLocators():
+    #Log in field
+    LOGIN_EMAIL = (By.NAME, 'login-username')
+    LOGIN_PASSWORD = (By.NAME, 'login-password')
+    LOGIN_BUTTON = (By.NAME, 'login_submit')
+
+    #Register field
+    REGISTER_EMAIL = (By.NAME, 'registration-email')
+    REGISTER_PASSWORD = (By.NAME, 'registration-password1')
+    REGISTER_CONFIRM = (By.NAME, 'registration-password2')
+    REGISTER_BUTTON = (By.NAME, 'registration_submit')
