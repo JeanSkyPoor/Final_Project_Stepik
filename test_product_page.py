b444bfb5e66493b77bba73be59fd48e48b2f8bfb
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-04 14:56:49 +0300

    add three new tests

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ from pages.product_page import ProductPage
 import time
 import pytest
 import json
+from pages.locators import ProductPageLocators
 
 with open('page_links.json') as f:
     data = json.load(f)['links']
@@ -16,3 +17,28 @@ def test_guest_can_add_product_to_basket(browser, link) -> None:
     page.check_item_name()
     page.check_item_price()
     
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser) -> None:
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE),\
+        "Success message is presented, but should not be"
+
+
+def test_guest_cant_see_success_message(browser) -> None:
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE),\
+        "Success message is presented, but should not be"
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE),\
+        "Success message is not disappeared"
