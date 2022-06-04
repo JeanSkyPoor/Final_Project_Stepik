be887fbbea685042b8ef9ca3002624809463251d
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-04 14:20:58 +0300

    update file

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,12 +5,12 @@ from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
+    def go_to_login_page(self) -> None:
         self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
         alert = self.browser.switch_to.alert
         alert.accept()
 
-        
-    def should_be_login_link(self):
+
+    def should_be_login_link(self) -> None:
         self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not presented'
 
