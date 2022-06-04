9d10ae835d6e1ebc8df4336a664b703f2c52e95f
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-04 16:21:00 +0300

    update BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,14 @@
+from os import link
+from xmlrpc.client import Boolean
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import math
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pages.basket_page import BasketPage
+from pages.locators import BasePageLocators
+
 
 class BasePage():
     def __init__(self, browser, url, timeout=10) -> None:
@@ -16,7 +21,7 @@ class BasePage():
         self.browser.get(self.url)
 
 
-    def is_element_present(self, how, what) -> None:
+    def is_element_present(self, how, what) -> Boolean:
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
@@ -39,7 +44,7 @@ class BasePage():
             print("No second alert presented")
 
 
-    def is_not_element_present(self, how, what, timeout=4) -> None:
+    def is_not_element_present(self, how, what, timeout=4) -> Boolean:
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
         except TimeoutException:
@@ -48,7 +53,7 @@ class BasePage():
         return False
 
 
-    def is_disappeared(self, how, what, timeout=4) -> None:
+    def is_disappeared(self, how, what, timeout=4) -> Boolean:
         try:
             WebDriverWait(self.browser, timeout, 1, TimeoutException).\
                 until_not(EC.presence_of_element_located((how, what)))
@@ -56,3 +61,16 @@ class BasePage():
             return False
 
         return True
+
+
+    def go_to_login_page(self) -> None:
+        self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID).click()
+
+
+    def should_be_login_link(self) -> None:
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), \
+            "Login link is not presented"
+
+
+    def open_basket(self) -> None:
+        self.browser.find_element(*BasePageLocators.BASKET_LINK).click()
