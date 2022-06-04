6c3723d78e588be530a79fc1b6d049c3fbc5cf3c
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-04 13:23:55 +0300

    add alert_accept()

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,7 +7,10 @@ from pages.locators import MainPageLocators
 class MainPage(BasePage):
     def go_to_login_page(self):
         self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
 
+        
     def should_be_login_link(self):
         self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not presented'
 
