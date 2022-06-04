88c18ed3dac13da3fe2e52a00cc6a5bc45d4c5df
Jean_Sky_Poor <katovano2705@gmail.com>
2022-06-04 13:44:30 +0300

    update file

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,23 +1,22 @@
-from distutils.log import Log
 from .base_page import BasePage
 from pages.locators import LoginPageLocators
 
 class LoginPage(BasePage):
-    def should_be_login_page(self):
+    def should_be_login_page(self) -> None:
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self):
+    def should_be_login_url(self) -> None:
         assert 'login' in self.browser.current_url
 
-    def should_be_login_form(self):
+    def should_be_login_form(self) -> None:
         assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON)
         assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL)
         assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD)
         
 
-    def should_be_register_form(self):
+    def should_be_register_form(self) -> None:
         assert self.is_element_present(*LoginPageLocators.REGISTER_EMAIL)
         assert self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD)
         assert self.is_element_present(*LoginPageLocators.REGISTER_CONFIRM)
